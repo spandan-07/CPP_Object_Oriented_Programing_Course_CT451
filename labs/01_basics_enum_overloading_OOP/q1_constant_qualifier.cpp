63e1fc2e995b1871fa2d01cc1efd8c6d2ea39300
Yuil <yuiltripathee79@gmail.com>
2022-01-09 20:15:58 +0545

    Add question 1 lab 1

---
+++ labs/01_basics_enum_overloading_OOP/q1_constant_qualifier.cpp
@@ -0,0 +1,20 @@
+/*q1_constant_qualifiers.cpp
+ *
+ * WAP using constant qualifiers
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+#define PI 3.14159
+
+int main(int argc, char const* argv[]) {
+  const int a = 5;
+
+  cout << "Constant a: " << a << endl;
+  cout << "Constant PI: " << PI << endl;
+
+  return 0;
+}
