c7c09a1b60bf7ec104ca39509df5360f1b8540a6
Spandan Devkota <spd8293@gmail.com>
2022-01-27 14:53:10 +0545

    Update q4_class_objects.cpp

---
+++ labs/01_basics_enum_overloading_OOP/q4_class_objects.cpp
@@ -0,0 +1,60 @@
+/*
+q3_area_using_function_overloading
+*/
+
+#include<iostream>
+#include<stdlib.h>
+#include<complex.h>
+using namespace std;
+
+class functionoverload
+{   
+    double a,b,c;
+    float s;
+    public: 
+    double area(double a){
+        return(a*a);
+    }
+    double area(double l, double b){
+        return(a*b);
+    }
+    double area(double a, double b, double c){
+        s=(a+b+c)/2;
+        return(sqrt(s*(s-a)*(s-b)*(s-c)));
+    }
+
+};
+
+
+int main(){
+    double x,y,z;
+    int ans;
+    functionoverload f1;
+    cout<<"Enter the no. of sides: ";
+    cin>>ans;
+    cout<<endl;
+   if (ans==1)
+   {
+       cout<<"Enter a side: ";
+       cin>>x;
+       cout<<"The area of square is "<<f1.area(x)<<endl;
+   }
+   else if (ans==2)
+   {
+       cout<<"Enter two sides: ";
+       cin>>x>>y;
+       cout<<"The area of rectangle is "<<f1.area(x,y)<<endl;
+   }
+   else if (ans==3)
+   {
+       cout<<"Enter three sides: ";
+       cin>>x>>y>>z;
+       cout<<"The area of triangle is "<<f1.area(x,y,z)<<endl;
+   }
+   else
+   {
+       cout<<"Invalid !!!";
+   }
+   
+return 0;
+}
